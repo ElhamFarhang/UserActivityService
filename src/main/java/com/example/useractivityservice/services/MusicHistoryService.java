0989343238ad128487de73b8792300aa03d66155
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-11-25 08:50:44 +0100

    update UserActivity & Database

--- src/main/java/com/example/useractivityservice/services/MusicHistoryService.java
+++ src/main/java/com/example/useractivityservice/services/MusicHistoryService.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 //--------------------- Elham - MusicHistoryService --------------
